140d70f749a3904c7f40e8753861ecb64d20066a
Preet Ramanuj <preetramanuj1001@gmail.com>
2026-08-19 09:34:59 +0530

    C code 19/8/26

---
+++ Other code/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() 
+{
+    char a[100];     
+    int b,c;
+    printf("Enter Name:");
+    scanf("%s",&a);
+    printf("%s",a);
+    return 0;
+    printf("Enter num 1 & num 2:");
+    scanf("%d%d",&b,&c);
+    printf("%d",b+c);
+    
+
+}
