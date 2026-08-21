2f51ed0af21c8811726bac60ef4e86b33350039c
Preet Ramanuj <preetramanuj1001@gmail.com>
2026-08-21 09:42:26 +0530

    C programe 21/8/26

---
+++ while loop/Armstrong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int temp,original,num,result;
+	printf("Enter Number:");
+	scanf("%d",&num);
+	original=num;
+	
+	while(num>0)
+	{
+		result=num%10;
+		temp = temp+ (result*result*result);
+		num /=10;
+	}
+	if(temp==original)
+	{
+		printf("Number is Armstrong");
+	}
+	else
+	{
+		printf("Number is not Amstrong");
+	}
+}
+
